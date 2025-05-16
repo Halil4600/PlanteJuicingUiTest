fdbfa0ed87ee375de2105cad3b5540d97135ca19
StureDan <std001@edu.zealand.dk>
2025-05-16 14:32:44 +0200

    selenium dank

--- PlanteJuicingUiTest/UnitTest1.cs
+++ PlanteJuicingUiTest/UnitTest1.cs
@@ -2,6 +2,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.ObjectModel;
 
@@ -40,13 +41,13 @@ namespace PlanteJuicingUITest
 
 
             IWebElement moistureMessage = _driver.FindElement(By.Id("moistureMessage"));
-            Assert.IsTrue(moistureMessage.Text.Contains("Du skal vande din plante!") || moistureMessage.Text.Contains("Din plante har det fint!"));
+            Assert.IsTrue(moistureMessage.Text.Contains("Du skal vande din plante!") || moistureMessage.Text.Contains("") || moistureMessage.Text.Contains("Din plante har det fint!"));
 
             IWebElement tempMessage = _driver.FindElement(By.Id("tempMessage"));
-            Assert.IsTrue(tempMessage.Text.Contains("Temperaturen er for lav!") || tempMessage.Text.Contains("Temperaturen er for høj!") || tempMessage.Text.Contains("Temperaturen er perfekt!"));
+            Assert.IsTrue(tempMessage.Text.Contains("Temperaturen er for lav!") || tempMessage.Text.Contains("") || tempMessage.Text.Contains("Temperaturen er for høj!") || tempMessage.Text.Contains("Temperaturen er perfekt!"));
 
             IWebElement waterLevelMessage = _driver.FindElement(By.Id("waterLevelMessage"));
-            Assert.IsTrue(waterLevelMessage.Text.Contains("Vandstanden er for lav!") || waterLevelMessage.Text.Contains("Vandstanden er for høj!") || waterLevelMessage.Text.Contains("Vandstanden er passende!"));
+            Assert.IsTrue(waterLevelMessage.Text.Contains("Vandstanden er for lav!") || waterLevelMessage.Text.Contains("") || waterLevelMessage.Text.Contains("Vandstanden er for høj!") || waterLevelMessage.Text.Contains("Vandstanden er passende."));
 
             // PlanteGuideTests
             IWebElement PlanteGuideButton = _driver.FindElement(By.Id("PlanteGuide"));
@@ -54,44 +55,97 @@ namespace PlanteJuicingUITest
             Assert.AreEqual("PlanteGuide", _driver.Title);
 
             // Tester om den første plante i vores PlanteGuide passer
-            wait.Until(d => d.FindElements(By.CssSelector(".list-group-item")).Count > 0);
+            // Vent på at <ul> elementet bliver synligt
+            IWebElement plantList = wait.Until(d => d.FindElement(By.Id("plant-list")));
+            wait.Until(d => plantList.Displayed);
 
-            // Find den første plante
-            var forstePlante = _driver.FindElement(By.CssSelector(".list-group-item:first-child a"));
-            string planteNavn = forstePlante.Text;
+            // Find alle <li> elementer (planter) i listen
+            var firstPlant = plantList.FindElements(By.TagName("li")).First();
+            var nameElement = firstPlant.FindElement(By.TagName("a"));
+            var planteNavn = nameElement.Text;
+            Assert.AreEqual(planteNavn, "European Silver Fir");
 
-            // Her kan du erstatte "Forventet plantenavn" med det navn du forventer
-            string forventetNavn = "European Silver Fir";
+            var addButton = firstPlant.FindElement(By.TagName("button"));
+            addButton.Click();
 
-            // Assert at plantens navn matcher det forventede
-            Assert.AreEqual(forventetNavn, planteNavn, "Den første plantes navn matcher ikke det forventede navn");
-
-            var firstAddButton = wait.Until(d => d.FindElement(By.CssSelector(".add-button")));
-            firstAddButton.Click();
-
-            var triggerButton = _driver.FindElement(By.Id("triggerPopup")); // Antager, at knappen har id="triggerPopup"
-            triggerButton.Click();
+            //Alert knappen
+            wait.Until(d => _driver.SwitchTo().Alert());
+            IAlert alert = _driver.SwitchTo().Alert();
+            alert.Accept();
 
             // Test af "Back" knappen
-            IWebElement BackButton = _driver.FindElement(By.Id("Back"));
-            BackButton.Click();
-            Assert.AreEqual("PlanteJuicing", _driver.Title);
+            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            IWebElement backButton = wait.Until(d => d.FindElement(By.Id("Back")));
+            backButton.Click();
+            Assert.AreEqual("Plante Juicing", _driver.Title);
 
             IWebElement PlantTable = wait.Until(d => d.FindElement(By.Id("AllPlants")));
             ReadOnlyCollection<IWebElement> rows = PlantTable.FindElements(By.TagName("tr"));
             IWebElement firstRow = rows[0];
             IWebElement newestRow = rows.Last();
             string PlantName = newestRow.FindElement(By.XPath(".//td[2]")).Text;
-            
+
             // Test af "Detaljer" knappen
-            IWebElement DetaljeButton = _driver.FindElement(By.Id("Detaljer"));
-            DetaljeButton.Click();
+            var buttons = _driver.FindElements(By.ClassName("details-button"));
+            buttons[0].Click();
+            wait.Until(d => _driver.Title.Contains("Plante Detaljer"));
             Assert.AreEqual("Plante Detaljer", _driver.Title);
 
-            // Test af "Slet" knappen
-            IWebElement DeleteButton = _driver.FindElement(By.Id("Slet"));
-            DeleteButton.Click();
+            // Back to drivhus
+            var buttons2 = _driver.FindElements(By.ClassName("back-button"));
+            buttons2[0].Click();
+            wait.Until(d => _driver.Title.Contains("Plante Juicing"));
             Assert.AreEqual("Plante Juicing", _driver.Title);
+
+
+            // Test af "Slet" knappen
+            var buttons3 = _driver.FindElements(By.ClassName("delete-button"));
+            buttons3[0].Click();
+            wait.Until(d => _driver.SwitchTo().Alert());
+            IAlert alert2 = _driver.SwitchTo().Alert();
+            alert2.Accept();
+            wait.Until(d => _driver.SwitchTo().Alert());
+            IAlert alert3 = _driver.SwitchTo().Alert();
+            alert3.Accept();
+            Assert.AreEqual("Plante Juicing", _driver.Title);
+            bool messageFound = _driver.PageSource.Contains("Ingen planter tilføjet endnu.");
+            Assert.IsTrue(messageFound, "The message 'Ingen planter tilføjet endnu.");
+
+            // Test af søgefunktion
+            IWebElement PlanteGuideButton2 = _driver.FindElement(By.Id("PlanteGuide"));
+            PlanteGuideButton2.Click();
+            Assert.AreEqual("PlanteGuide", _driver.Title);
+            IWebElement searchBox = wait.Until(d => d.FindElement(By.Id("searchBox")));
+            searchBox.SendKeys("apple");
+            var searchButton = _driver.FindElement(By.Id("searchButton"));
+            searchButton.Click();
+            IWebElement plantList2 = wait.Until(d => d.FindElement(By.Id("plant-list")));
+            wait.Until(d => plantList2.Displayed);
+            var firstPlant2 = plantList2.FindElements(By.TagName("li")).First();
+            var nameElement2 = firstPlant2.FindElement(By.TagName("a"));
+            var planteNavn2 = nameElement2.Text;
+            Assert.AreEqual(planteNavn2, "Strawberry Tree");
+
+            // Ryd søgefunktionen
+            var clearSearchButton = _driver.FindElement(By.Id("clearSearchButton"));
+            clearSearchButton.Click();
+            IWebElement searchBox2 = wait.Until(d => d.FindElement(By.Id("searchBox")));
+            string placeholder = searchBox2.GetAttribute("placeholder");
+            Assert.AreEqual(placeholder, "Søg efter planter...");
+
+            // Næste side test
+            IWebElement currentPageInfo = wait.Until(d => d.FindElement(By.Id("currentPageInfo")));
+            Assert.IsTrue(currentPageInfo.Text.Contains("Side 1 af 337"));
+            var nextPage = wait.Until(d => d.FindElement(By.Id("nextPage")));
+            nextPage.Click();
+            IWebElement currentPageInfo2 = wait.Until(d => d.FindElement(By.Id("currentPageInfo")));
+            Assert.IsTrue(currentPageInfo2.Text.Contains("Side 2 af 337"));
+
+            // Forrige side test
+            var prevPage = wait.Until(d => d.FindElement(By.Id("prevPage")));
+            prevPage.Click();
+            IWebElement currentPageInfo3 = wait.Until(d => d.FindElement(By.Id("currentPageInfo")));
+            Assert.IsTrue(currentPageInfo3.Text.Contains("Side 1 af 337"));
 
 
         }
